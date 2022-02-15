cd874dd17f4d1cb57e82c3992b5d910bb701e1b4
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-02-15 19:28:16 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
-const gridContainer = document.getElementById("grid-container")
+const gridContainer = document.getElementById("grid-container");
 let nextWord;
 let nextWordArray = [];
 let numberOfLettersV1;
 let numberOfLettersV2;
 let nextLetter;
-let potentialLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let potentialLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 let guessedLetters = [];
-let alphabet= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 let correctAnswer = [];
 let checkingCorrectAnswer = [];
 let nextLetterV2;
@@ -15,9 +69,10 @@ let placeOfGuessedWord;
 let blankSpacesArray;
 let placeOfGuessedNumberV2;
 let placeOfGuessedNumberV3;
-let FirstBlankSpace
+let FirstBlankSpace;
 let firstLetterOfArray;
 let nextWordArrayLetter;
+let numberOfGuesses = 0;
 
 //essentials
 // step 1: go to a corner in a room (any room)
@@ -25,95 +80,131 @@ let nextWordArrayLetter;
 // step 3: happiness
 
 for (let i = 0; i < 26; i++) {
-    const gridItem = document.createElement("p");
-    gridItem.classList.add("grid-item");
-    gridItem.setAttribute("id", alphabet[i]);
-    gridItem.innerText = alphabet[i];
-    gridContainer.appendChild(gridItem);
+  const gridItem = document.createElement("p");
+  gridItem.classList.add("grid-item");
+  gridItem.setAttribute("id", alphabet[i]);
+  gridItem.innerText = alphabet[i];
+  gridContainer.appendChild(gridItem);
 }
 
 function setNextWordToString() {
-    nextWord = document.getElementById("nextWordInput").value;
-    alert("New game started");
-    nextWordArray = nextWord.split("");
-    console.log(nextWordArray);
-    document.getElementById("nextWordInput").style.display = "none";
-    document.getElementById("nextWordInputButton").style.display = "none";
-    setTheBlankSpaces();
+  nextWord = document.getElementById("nextWordInput").value;
+  alert("New game started");
+  nextWordArray = nextWord.split("");
+  console.log(nextWordArray);
+  document.getElementById("nextWordInput").style.display = "none";
+  document.getElementById("nextWordInputButton").style.display = "none";
+  setTheBlankSpaces();
 }
 
- function setTheBlankSpaces() {
-    numberOfLettersV1 = nextWordArray.length;
-    console.log(numberOfLettersV1);
-    numberOfLettersV2 = parseInt(numberOfLettersV1);
-    console.log(numberOfLettersV2);
-    let blankSpaces = "";
-    for (let i=0; i < numberOfLettersV2; i++) {
-        if (nextWord[i] === " ") {
-            blankSpaces += " space ";
-        } else {
-            blankSpaces += "_";
-
-        }
+function setTheBlankSpaces() {
+  numberOfLettersV1 = nextWordArray.length;
+  console.log(numberOfLettersV1);
+  numberOfLettersV2 = parseInt(numberOfLettersV1);
+  console.log(numberOfLettersV2);
+  let blankSpaces = "";
+  for (let i = 0; i < numberOfLettersV2; i++) {
+    if (nextWord[i] === " ") {
+      blankSpaces += " space ";
+    } else {
+      blankSpaces += "_";
     }
-    blankSpacesArray = blankSpaces.split("");
-    nextWordArrayLetter = nextWordArray;
-    firstLetterOfArray = nextWordArrayLetter.splice(0,1);
-    nextWordArray.splice(0, 0, firstLetterOfArray);
-    blankSpacesArray.splice(0, 1);
-    blankSpacesArray.splice(0, 0, firstLetterOfArray);
-    console.log("log blank spaces");
-    console.log(blankSpacesArray);
-    document.getElementById("blankSpaces").innerHTML = blankSpacesArray;
+  }
+  blankSpacesArray = blankSpaces.split("");
+  nextWordArrayLetter = nextWordArray;
+  firstLetterOfArray = nextWordArrayLetter.splice(0, 1);
+  nextWordArray.splice(0, 0, firstLetterOfArray);
+  blankSpacesArray.splice(0, 1);
+  blankSpacesArray.splice(0, 0, firstLetterOfArray);
+  console.log("log blank spaces");
+  console.log(blankSpacesArray);
+  document.getElementById("blankSpaces").innerHTML = blankSpacesArray;
 }
- 
+
 function nextLetterGuess() {
-    nextLetter = document.getElementById("nextLetterInput").value;
-    console.log(checkingCorrectAnswer + "checking correct answer");
-    checkingCorrectAnswer.splice(0, 0, nextLetter)
-    console.log(checkingCorrectAnswer);
-    showAlreadyInputedLetters();
+  nextLetter = document.getElementById("nextLetterInput").value;
+  console.log(checkingCorrectAnswer + "checking correct answer");
+  checkingCorrectAnswer.splice(0, 0, nextLetter);
+  console.log(checkingCorrectAnswer);
+  showAlreadyInputedLetters();
 }
 
 function showAlreadyInputedLetters() {
-    if (potentialLetters.includes(nextLetter)) {
-        const highlightedLetter = document.getElementById(nextLetter)
-        highlightedLetter.classList.add("highlighted")
-    }
-    checkingCorrectAnswer.splice()
-    clearInputArea();
+  if (potentialLetters.includes(nextLetter)) {
+    const highlightedLetter = document.getElementById(nextLetter);
+    highlightedLetter.classList.add("highlighted");
+  }
+  checkingCorrectAnswer.splice();
+  clearInputArea();
 }
 
 function clearInputArea() {
-    console.log("clearing letter input");
-    document.getElementById("nextLetterInput").value = "";
-    checkIfCorrectGuess();
+  console.log("clearing letter input");
+  document.getElementById("nextLetterInput").value = "";
+  checkIfCorrectGuess();
 }
 
 function checkIfCorrectGuess() {
-    placeOfGuessedWord = nextWordArray.indexOf(nextLetter);
-    placeOfGuessedNumberV2 = parseInt(placeOfGuessedWord)
-    console.log(nextWordArray.indexOf(nextLetter));
-    //todo: fix if statement not running correctly
-    console.log("if statement log");
+  placeOfGuessedWord = nextWordArray.indexOf(nextLetter);
+  placeOfGuessedNumberV2 = parseInt(placeOfGuessedWord);
+  console.log(nextWordArray.indexOf(nextLetter));
+  console.log("if statement log");
+  console.log(placeOfGuessedWord);
+  if (placeOfGuessedWord >= 0) {
+    console.log("correct");
     console.log(placeOfGuessedWord);
-    if (placeOfGuessedWord >= 0) {
-        console.log("correct");
-        console.log(placeOfGuessedWord);
-        console.log(nextWordArray);
-        console.log(blankSpacesArray);
-        changeTheBlankSpaces();
-        checkIfCorrectWordGuess();
-    }
+    console.log(nextWordArray);
+    console.log(blankSpacesArray);
+    changeTheBlankSpaces();
+    checkIfCorrectWordGuess();
+  } else {
+    numberOfGuesses++;
+    console.log(numberOfGuesses);
+    showHangmanBodyParts();
+  }
+}
+
+function showHangmanBodyParts() {
+  switch (numberOfGuesses) {
+    case 1:
+      document.getElementById("hangmanHead").classList.remove("incorrectGuess");
+      break;
+    case 2:
+      document.getElementById("hangmanBody").classList.remove("incorrectGuess");
+      break;
+    case 3:
+      document
+        .getElementById("hangmanLeftHand")
+        .classList.remove("incorrectGuess");
+      break;
+    case 4:
+      document
+        .getElementById("hangmanRightHand")
+        .classList.remove("incorrectGuess");
+      break;
+    case 5:
+      document
+        .getElementById("hangmanLeftLeg")
+        .classList.remove("incorrectGuess");
+      break;
+    case 6:
+      document
+        .getElementById("hangmanRightLeg")
+        .classList.remove("incorrectGuess");
+      alert("You lose");
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+      break;
+  }
 }
 
 function changeTheBlankSpaces() {
-    //todo: fix blank spaces not changing
-    blankSpacesArray.splice(placeOfGuessedWord, 1, nextLetter);
-    console.log(blankSpacesArray);
-    document.getElementById("blankSpaces").innerHTML = blankSpacesArray;
+  blankSpacesArray.splice(placeOfGuessedWord, 1, nextLetter);
+  console.log(blankSpacesArray);
+  document.getElementById("blankSpaces").innerHTML = blankSpacesArray;
 }
 
 function checkIfCorrectWordGuess() {
-    console.log(checkingCorrectAnswer);
+  console.log(checkingCorrectAnswer);
 }
